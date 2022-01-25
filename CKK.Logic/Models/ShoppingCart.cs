f686bdb14db7af47cda144645e2d4c6ed956ade3
telaner <telane.hill@gmail.com>
2022-01-24 19:13:31 -0700

    4a deliverable

---
+++ CKK.Logic/Models/ShoppingCart.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKK.Logic.Models
+{
+    public class ShoppingCart
+    {
+        private Customer _customer;
+        private ShoppingCartItem _product1;
+        private ShoppingCartItem _product2;
+        private ShoppingCartItem _product3;
+
+        public ShoppingCart(Customer cust) 
+        {
+            _customer = cust;
+        }
+
+        
+        public int GetCustomerID() 
+        {
+            return _customer.GetId();
+        }
+
+        public ShoppingCartItem GetProductByID(int id)
+        {
+           if(_product1.GetProduct().GetId().Equals(id))
+                return _product1;
+           if (_product2.GetProduct().GetId().Equals(id))
+                return _product2;
+           if (_product3.GetProduct().GetId().Equals(id))
+                return _product3;
+           else
+                return null;
+        }
+        public ShoppingCartItem AddProduct(Product prod, int Quantity) 
+        {
+            if (Quantity < 1)
+                return null;   
+            
+            if (_product1 != null && _product1.GetProduct().GetId()==(prod.GetId()))
+            {
+                _product1.SetQuantity(_product1.GetQuantity()+Quantity);
+                return _product1;
+            }
+            
+            if (_product2 != null && _product2.GetProduct().GetId() == (prod.GetId()))
+            {
+                _product2.SetQuantity(_product2.GetQuantity() + Quantity);
+                return _product2;
+            }
+            
+            if (_product3 != null && _product3.GetProduct().GetId() == (prod.GetId()))
+            {
+                _product3.SetQuantity(_product3.GetQuantity() + Quantity);
+                return _product3;
+            }
+
+            if (_product1 == null)
+            {
+                _product1 = new ShoppingCartItem(prod, Quantity);
+                return _product1;
+            }
+
+            if (_product2 == null) 
+            {
+                _product2 = new ShoppingCartItem(prod, Quantity);
+                return _product2;
+            }
+
+            if (_product3 == null)
+            {
+                _product3 = new ShoppingCartItem(prod, Quantity);
+                return _product3;
+            }
+
+            else
+                return null;
+        }
+        public ShoppingCartItem AddProduct(Product prod)
+        {
+            return AddProduct
+                (prod, 1);
+        }
+        public ShoppingCartItem RemoveProduct(Product prod, int Quantity) 
+        {
+            if (Quantity < 1)
+                return null;
+
+            else if (_product1 != null && _product1.GetProduct().GetId() == (prod.GetId()))
+            {
+                _product1.SetQuantity(_product1.GetQuantity() - Quantity);
+                return _product1;
+            }
+
+            else if (_product2 != null && _product2.GetProduct().GetId() == (prod.GetId()))
+            {
+                _product2.SetQuantity(_product2.GetQuantity() - Quantity);
+                return _product2;
+            }
+
+            else if (_product3 != null && _product3.GetProduct().GetId() == (prod.GetId()))
+            {
+                _product3.SetQuantity(_product3.GetQuantity() - Quantity);
+                return _product3;
+            }
+            else
+                return null;
+        }
+        public decimal GetTotal() 
+        {
+            decimal total = 0;
+            if (_product1 != null) total += _product1.GetTotal();
+            if (_product2 != null) total += _product2.GetTotal();
+            if (_product3 != null) total += _product3.GetTotal();
+            return total;
+        }
+        public ShoppingCartItem GetProduct(int prodNum)
+        {
+            if (prodNum == 1)
+                return _product1;
+            if (prodNum == 2)
+                return _product2;
+            if (prodNum == 3)
+                return _product3;
+            else
+                return null;
+
+        }
+
+    }
+}
---
+++ CKK.tests/CKK.tests.cs
@@ -0,0 +1,124 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CKK.Logic.Models;
+
+
+namespace CKK.tests
+{
+    [TestClass]
+    public class CKKTests
+    {
+        [TestMethod]
+        public void AddingProduct()
+        {
+            Customer customer = new Customer();
+            customer.SetName("John Doe");
+            customer.SetId(456);
+            customer.SetAddress("123 Main Street");
+
+            Product _product1 = new Product();
+            _product1.SetName("Toy Boat");
+            _product1.SetId(123);
+            _product1.SetPrice(3);
+            int product1quantity = 2;
+
+            Product _product2 = new Product();
+            _product2.SetName("Necklace");
+            _product2.SetId(213);
+            _product2.SetPrice(6);
+            int product2quantity = 3;
+
+            Product _product3 = new Product();
+            _product3.SetName("Hat");
+            _product3.SetId(312);
+            _product3.SetPrice(4);
+            int product3quantity = 2;
+
+
+            ShoppingCart shoppingCart = new(customer);
+
+            var actual = shoppingCart.AddProduct(_product1, product1quantity);
+            actual = shoppingCart.AddProduct(_product1, 1);
+
+            Assert.AreEqual(3, actual.GetQuantity());
+
+        }
+
+        [TestMethod]
+        public void RemovingProduct()
+        {
+            Customer customer = new Customer();
+            customer.SetName("John Doe");
+            customer.SetId(456);
+            customer.SetAddress("123 Main Street");
+
+            Product _product1 = new Product();
+            _product1.SetName("Toy Boat");
+            _product1.SetId(123);
+            _product1.SetPrice(3);
+            int product1quantity = 2;
+
+            Product _product2 = new Product();
+            _product2.SetName("Necklace");
+            _product2.SetId(213);
+            _product2.SetPrice(6);
+            int product2quantity = 3;
+
+            Product _product3 = new Product();
+            _product3.SetName("Hat");
+            _product3.SetId(312);
+            _product3.SetPrice(4);
+            int product3quantity = 2;
+
+
+            ShoppingCart shoppingCart = new(customer);
+
+
+            shoppingCart.AddProduct(_product2, product2quantity);
+            var actual = shoppingCart.RemoveProduct(_product2, 1);
+
+            Assert.AreEqual(2, actual.GetQuantity());
+
+        }
+
+        [TestMethod]
+        public void GettingTotal()
+        {
+            Customer customer = new Customer();
+            customer.SetName("John Doe");
+            customer.SetId(456);
+            customer.SetAddress("123 Main Street");
+
+            Product _product1 = new Product();
+            _product1.SetName("Toy Boat");
+            _product1.SetId(123);
+            _product1.SetPrice(3);
+            int product1quantity = 2;
+
+            Product _product2 = new Product();
+            _product2.SetName("Necklace");
+            _product2.SetId(213);
+            _product2.SetPrice(6);
+            int product2quantity = 3;
+
+            Product _product3 = new Product();
+            _product3.SetName("Hat");
+            _product3.SetId(312);
+            _product3.SetPrice(4);
+            int product3quantity = 2;
+            
+            ShoppingCart shoppingCart = new(customer);
+
+            shoppingCart.AddProduct(_product1, product1quantity);
+
+            shoppingCart.AddProduct(_product2, product2quantity);
+
+            shoppingCart.AddProduct(_product3, product3quantity);
+
+
+            var actual = shoppingCart.GetTotal();
+
+            Assert.AreEqual(32, actual);
+            
+        }
+    }
+    }
